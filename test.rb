71969ae8438403a4ba80b550d2b63b56ea7b5701
河田鷹介 <benefium@gmail.com>
2022-11-27 21:39:07 +0900

    for yasl_aff_param

--- test.rb
+++ test.rb
@@ -2,44 +2,85 @@ TEXT_RED = "\e[31m"
 TEXT_GREEN = "\e[32m"
 TEXT_RESET = "\e[m"
 
-arg = {
-  exe: "yasl_hw",
-  cases: [
-    {
-      name: "basic",
-      in: [],
-      out: [/^Hello world\n$/],
-    },
-    {
-      name: "with an arg",
-      in: ["apple"],
-      out: [/^Hello world\n$/],
+Programs = [
+  {
+    exe: "yasl_hw",
+    cases: [
+      {
+        name: "basic",
+        in: [],
+        out: [/^Hello world\n$/],
       },
-    {
-      name: "with two args",
-      in: ["apple", "banana"],
-      out: [/^Hello world\n$/],
-    },
-  ],
-}
+      {
+        name: "with an arg",
+        in: ["apple"],
+        out: [/^Hello world\n$/],
+        },
+      {
+        name: "with two args",
+        in: ["apple", "banana"],
+        out: [/^Hello world\n$/],
+      },
+    ],
+  },
+
+  {
+    exe: "yasl_aff_param",
+    cases: [
+      {
+        name: "no arg",
+        in: [],
+        out: [],
+      },
+      {
+        name: "with an arg",
+        in: ["apple"],
+        out: ["apple"],
+      },
+      {
+        name: "with a blank arg",
+        in: [""],
+        out: [""],
+      },
+      {
+        name: "with two args",
+        in: ["apple", "banana"],
+        out: ["apple", "banana"],
+      },
+      {
+        name: "with separated args",
+        in: ["apple", "banana latte", "chai    latte"],
+        out: ["apple", "banana latte", "chai    latte"],
+      },
+    ],
+  },
+]
 
 def launch(script_name, cases)
   kos = 0
-  puts "[#{script_name}]"
   cases.each_with_index{ |c, i|
     begin
       print "#{c[:name]} ... "
-      result = (`./yasl #{script_name} #{c[:in].join(" ")}; echo $?`).split(/(?<=\n)/)
+      # 実行
+      result = (`./yasl #{script_name} #{c[:in].map{ |a| "\"#{a}\""}.join(" ")}; echo $?`).split(/(?<=\n)/)
+      # 終了ステータスだけを分離
       actual_exit = result.pop.to_i
+      # 出力を比較
+      expected_lines = c[:out]
       result.each_with_index { |actual_line, l|
-        expected_line = c[:out][l]
+        expected_line = expected_lines[l]
+        # 出力行数 > 想定行数 の場合のエラー
         if !expected_line 
-          raise "result is shorted than expected!!"
+          raise "result is shorter than expected!!"
         end
         if !actual_line.match(expected_line)
           raise "l:#{l + 1} differs: expected: #{expected_line}, actual: \"#{actual_line.gsub(/\n/, "\\n")}\""
         end
       }
+      # 出力行数 < 想定行数 の場合のエラー
+      if result.size < expected_lines.size
+        raise "result is longer than expected!!"
+      end
       expected_exit = c[:exit] || 0
       if expected_exit != actual_exit
         raise "exit status differs: expected: #{expected_exit}, actual: #{actual_exit}"
@@ -58,5 +99,15 @@ def launch(script_name, cases)
   kos
 end
 
-launch(arg[:exe], arg[:cases])
+
+given, _ = ARGV
+
+Programs.each_with_index{ |prog, i|
+  script_name = prog[:exe]
+  next if given && given != script_name
+
+  puts "[#{script_name}]"
+  launch(prog[:exe], prog[:cases])
+}
+
 
